e81f2a6a6cbbfcd7246771590ae18444834681c3
ericadebem <110907550+ericadebem@users.noreply.github.com>
2022-09-26 23:34:00 +0200

    upload | Inicio

---
+++ script.js
@@ -0,0 +1,10 @@
+//Aula JS 
+
+
+let campo = document.querySelector('input[type=tel]'); 
+let lista = document.querySelectorAll('input[type=button]');
+// Percorrendo 
+lista: for(i=0;i<12;i++){ let tecla = lista[i]; tecla.onclick = function(){ campo.value = campo.value + tecla.value; } } 
+
+// Limpando Campos 
+let limpa = document.querySelector('input[type=reset]');
