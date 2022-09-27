5c4fb457d98c3eb78b276356f289e0525b4b8e2c
ericadebem <110907550+ericadebem@users.noreply.github.com>
2022-09-27 23:00:07 +0200

    iniciando o ajuste do botton reset

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 //Aula JS 
-
-
 let campo = document.querySelector('input[type=tel]'); 
 let lista = document.querySelectorAll('input[type=button]');
+
+
 // Percorrendo 
 lista: for(i=0;i<12;i++){ let tecla = lista[i]; tecla.onclick = function(){ campo.value = campo.value + tecla.value; } } 
 
